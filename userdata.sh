b37bdac3de5021443e2c41938d3cd0d2ddec32ec
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-31 01:38:00 +0530

    okko

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
-echo HELOOOOOO >> /opt/userdata.log
-echo HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO &>> /opt/userdata.log
+#!/bin/bash
+
+
 yum install ansible -y &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/roboshop-project-v1/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
