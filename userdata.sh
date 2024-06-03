6a0e79e88f60743610be154041f4a8b0c04d5664
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-06-03 23:03:29 +0530

    ok

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 yum install ansible -y &>>/opt/userdata.log
+pip3 install botocore boto3  &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/roboshop-project-v1/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
